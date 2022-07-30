c27a48ea7f7fb2cce29fdc4d4cf50036ff3f96ee
UniFail <vika.lolkova@mail.ru>
2022-07-30 17:36:39 +0700

    It is homework 2.7

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,70 @@
-package PACKAGE_NAME;public class Employee {
+import java.util.Objects;
+
+public class Employee {
+    private final String fio;
+    private float salary;
+    private int department;
+    public int id;
+    static int counter;
+
+    static {
+        counter = 1;
+    }
+
+    public Employee(String fio, float salary, int department) {
+        this.fio = fio;
+        this.salary = salary;
+        this.department = department;
+        id = counter++;
+    }
+
+    public String getFio() {
+
+        return fio;
+    }
+
+    public int getDepartment() {
+
+        return department;
+    }
+
+    public void setDepartment(int department) {
+
+        this.department = department;
+    }
+
+    public float getSalary() {
+
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+
+        this.salary = salary;
+    }
+    public int getId(){
+        return this.id++;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "\nEmployee " +
+                "{fio='" + fio + '\'' +
+                ", salary=" + salary +
+                ", department=" + department + "} ";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Float.compare(employee.salary, salary) == 0 && department == employee.department && id == employee.id && Objects.equals(fio, employee.fio);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fio, salary, department, id);
+    }
 }
--- src/EmployeeBook.java
+++ src/EmployeeBook.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class EmployeeBook {
+import java.util.*;
+
+public class EmployeeBook {
+    private final Map<String, String> employeeMap = new HashMap<>(Map.of());
+    private int size;
+
+    public EmployeeBook() {
+        size = 0;
+    }
+
+    public void printSeparator() {
+        System.out.println("=================================================");
+    }
+
+    public void printSeparator2() {
+        System.out.println("-------------------------------------------------");
+    }
+
+
+    public void addEmployee(Employee newEmployee) {
+        String value = " department = " + newEmployee.getDepartment() +
+                ", salary = " + newEmployee.getSalary();
+        employeeMap.put(newEmployee.getFio(), value);
+    }
+
+    public void printAllEmployee() {
+        System.out.println(employeeMap.toString());
+    }
+
+    public void removeEmployee(String fioEmployee) {
+        employeeMap.remove(fioEmployee);
+    }
+
+    public void findEmployee(String fioEmployee){
+        if (employeeMap.containsKey(fioEmployee)){
+            System.out.println(employeeMap.get(fioEmployee));
+        }
+        else {
+            System.out.println("Employee not found");
+        }
+    }
+
 }
+
+
